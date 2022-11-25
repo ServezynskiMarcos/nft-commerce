b0b917683ca259cafd8a33bf1c5ae139c14d480e
Marcos Servezynski <servezynskimarcos1@gmail.com>
2022-11-25 20:09:39 -0300

    path

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route exact path={"/"} element={<Home />} />
-        <Route path={"https://nft-commerce-tau.vercel.app/nft/:id"} element={<DetailCard />} />
+        <Route exact path={"/nft/:id"} element={<DetailCard />} />
       </Routes>
       <Footer />
     </Container>
