b963d740574bacc2ffd3d7e55e222ba38448c298
Marcos Servezynski <servezynskimarcos1@gmail.com>
2022-11-26 14:33:06 -0300

    add:(CryptoComponent)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Components/Home/Home";
 import Nav from "./Components/Nav/Nav";
 import { Route, Routes } from "react-router-dom";
 import DetailCard from "./Components/DetailCard/DetailCard";
+import Crypto from "./Components/Crypto/Crypto";
 function App() {
   return (
     <Container minW="full" m={0} p={0}>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path={"/"} element={<Home />} />
         <Route exact path={"/nft/:id"} element={<DetailCard />} />
+        <Route exact path={"/mercado"} element={<Crypto/>} />
       </Routes>
       <Footer />
     </Container>
---
+++ src/Components/Crypto/Crypto.jsx
@@ -0,0 +1,93 @@
+import {
+  Container,
+  Icon,
+  Image,
+  Stack,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BsStar } from "react-icons/bs";
+import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
+import theme from "../../theme";
+
+const Crypto = () => {
+  const [coins, setCoins] = useState([]);
+  const getData = async () => {
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true"
+    );
+    setCoins(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Container minW="full">
+      <Text
+        fontWeight={"bold"}
+        fontFamily={"primary"}
+        fontSize={{base: 'xs', md: "xl", lg: "2xl"}}
+        textAlign={"center"}
+        m={{base: 0, xl: 6}}
+        marginBottom={{base: 2, xl: 12}}
+      >
+        Precios de las criptomonedas según la capitalización del mercado
+      </Text>
+      <TableContainer>
+        <Table variant="simple" colorScheme={"secondary"}>
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>Moneda</Th>
+              <Th>Precio</Th>
+              <Th>Volumen en 24h</Th>
+              <Th>Últimos 7 días</Th>
+            </Tr>
+          </Thead>
+          {coins?.map((e) => {
+            return (
+              <Tbody>
+                <Td>
+                  <Stack direction={"row"} alignItems={"center"}>
+                    <Icon as={BsStar} />
+                    <Text>{e.market_cap_rank}</Text>
+                  </Stack>
+                </Td>
+                <Td>
+                  <Stack direction={"row"} alignItems={"center"}>
+                    <Image src={e.image} w={7} /> <Text>{e.name}</Text>
+                  </Stack>
+                </Td>
+                <Td>${e.current_price.toLocaleString("es-ES")}</Td>
+                <Td>${e.total_volume.toLocaleString("es-ES")}</Td>
+                <Td isNumeric>
+                  <Sparklines data={e.sparkline_in_7d.price}>
+                    <SparklinesLine
+                      color="teal"
+                      style={{ strokeWidth: 1, fill: "none" }}
+                    />
+                    <SparklinesSpots />
+                  </Sparklines>
+                </Td>
+              </Tbody>
+            );
+          })}
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default Crypto;
